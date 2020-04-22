60aa3405080b7f6ba0f39fa72f2d7c0f54766542
FluffyStyle <64157426+FluffyStyle@users.noreply.github.com>
2020-04-22 21:21:32 +0200

    Update funafuti-dashboard.user.js

--- funafuti-dashboard.user.js
+++ funafuti-dashboard.user.js
@@ -4,6 +4,7 @@
 // @author       Allure149
 // @description  FunaFuti-Dashboard von Meyerscheeeee & FluffyStyle
 // @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com)\/.*$/
+// @updateURL    https://github.com/FluffyStyle/lssscripte/blob/master/funafuti-dashboard.user.js
 // @grant        none
 // ==/UserScript==
 /* global $ */
